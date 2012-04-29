c563a2f58153180625cf9ca5695a77bba5d5c4c3
jakesquared <jakes@verticalbrands.com>
2012-04-29 15:09:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pets::Application.config.secret_token = 'c3f7d7f4154faa3b7ab56caae7333eab2050f415e217b337331f8ef155ccc80716203655124e6901b9b4136761d3c4818e62acfc0c736d187b7ac648e81ffd68'
